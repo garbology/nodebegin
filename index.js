64ddf49d6be02951ed45a7d27f524e57f0ba4153
Jay <jay@sensationalkids.com.au>
2013-08-02 23:34:52 +1000

    Simple routing added

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 server = require('./server');
-server.start();
+router = require('./router');
+requestHandlers = require('./requestHandlers');
+
+var handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+
+server.start(router.route, handle);
 
 
